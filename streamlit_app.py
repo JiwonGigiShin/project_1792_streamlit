97fd58f3e6dabb9395ececa80a622413392640ae
Jiwon Shin <109152749+JiwonGigiShin@users.noreply.github.com>
2024-10-21 14:58:25 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,11 @@
 import streamlit as st
 import pandas as pd
+#requirements.txt
+
+
 
 st.title("Hello Jayan, Anuprita, Omar!")
+st.markdown("this is streamlit website for le wagon")
+
+
+df = pd.read_csv('moderation.csv')
